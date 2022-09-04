LineageOS/android_kernel_oneplus_sm8350

treewide: Get rid of oplus feature flag setup
Change-Id: Ia09800876da95c855fbadcccd2d1f21431b97a62

drivers/net/wireless/cnss2/main.h
@@ -502,7 +502,7 @@ struct cnss_plat_data {
 	struct cnss_dms_data dms;
 	int power_up_error;
 	u32 hw_trc_override;
-#ifdef OPLUS_FEATURE_WIFI_DCS_SWITCH
+#ifdef CONFIG_OPLUS_FEATURE_WIFI_DCS_SWITCH
 	unsigned long loadBdfState;
 	unsigned long loadRegdbState;
 #endif
@@ -512,7 +512,7 @@ struct cnss_plat_data {
 	bool adsp_pc_enabled;
 };
 
-#ifdef OPLUS_FEATURE_WIFI_DCS_SWITCH
+#ifdef CONFIG_OPLUS_FEATURE_WIFI_DCS_SWITCH
 enum cnss_load_state {
 	CNSS_LOAD_BDF_FAIL = 1,
 	CNSS_LOAD_BDF_SUCCESS,

OnePlus-Nord-N20-5G-Dev/android_kernel_msm-5.4_oneplus_sm6375

drivers/soc/qcom/icnss2/main.h
@@ -465,14 +465,14 @@ struct icnss_priv {
 	struct icnss_dms_data dms;
 	u8 use_nv_mac;
 	u32 wlan_en_delay_ms;
-#ifdef OPLUS_FEATURE_SWITCH_CHECK
+#ifdef CONFIG_OPLUS_FEATURE_SWITCH_CHECK
 //Add for: check fw status for switch issue
 	unsigned long loadBdfState;
 	unsigned long loadRegdbState;
 #endif /* OPLUS_FEATURE_SWITCH_CHECK */
 };
 
-#ifdef OPLUS_FEATURE_SWITCH_CHECK
+#ifdef CONFIG_OPLUS_FEATURE_SWITCH_CHECK
 //Add for: check fw status for switch issue
 enum cnss_load_state {
 	CNSS_LOAD_BDF_FAIL = 1,